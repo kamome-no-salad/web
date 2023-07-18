884eeeb03bbe868c9798875b1ae89acc00310136
charl2k <yukinoko2@gmail.com>
2023-07-19 03:12:34 +0900

    環境構築 vite + react + js singlefile

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_PAGES
         ? "web"
         : "./",
-  plugins: [react(),viteSingleFile()],
+  plugins: [react()],
   build: {
     outDir: "docs",
   },
